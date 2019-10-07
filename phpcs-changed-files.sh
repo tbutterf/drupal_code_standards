8a218444418cf73574120578b35458674b5f084d
Nils Loewen <nloewen@acromedia.com>
2019-10-07 14:23:40 -0700

    Remove changed files script.

--- phpcs-changed-files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-main() {
-  files=$(git diff origin/master --name-only --diff-filter=ACM /home/nlz/commerce/wwwroot/shop/mobility/sites/all/modules/telus/)
-  for file in $files; do echo $file; done
-}
